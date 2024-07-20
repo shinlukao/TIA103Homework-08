2132ff12eb54c89c79b5a933b5aeeeae988cc981
Shin-lu <warren800905@gmail.com>
2024-07-20 12:34:05 +0800

    get lottery array 6 number

--- src/hw3/LotteryPro.java
+++ src/hw3/LotteryPro.java
@@ -1,5 +1,6 @@
 package hw3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LotteryPro {
@@ -18,23 +19,32 @@ public class LotteryPro {
 //		lotto.print(x);
 		System.out.println("總共有" + sum + "個數字");
 
-		System.out.println(y[]);
+		int n = 0;
+		while (n < 6) {  //由y[]裡取出6個數字，並將取過得值改成"0"
+			int strange = (int) ((Math.random() * sum) ) ;
+			if (y[strange] != 0) {
+				System.out.print(y[strange] + " ");
+				y[strange] = 0;
+				n++;
+			}
+		}
+
 	}
 
-	public int[] array() {
+	public int[] array() { // 拿到array data
 
 		int[] data = new int[arrayCount()];
-		int count =-1;
+		int count = -1;
 		count++;
 		for (int i = 1; i <= 49; i++) {
-			if (i / 10 != a && i % 10 != a) {	
-			 data[count++]= i;
+			if (i / 10 != a && i % 10 != a) {
+				data[count++] = i;
 			}
 		}
 		return data;
 	}
-	
-	public void print(int[] data) {
+
+	public void print(int[] data) { // 印出array data
 		for (int i = 1; i <= 49; i++) {
 			if (i / 10 != a && i % 10 != a) {
 				System.out.print(i + " ");
@@ -43,7 +53,7 @@ public class LotteryPro {
 		System.out.println();
 	}
 
-	public int arrayCount() {
+	public int arrayCount() { // 計算幾個索引值
 		int count = 0;
 		for (int i = 1; (i <= 49); i++) {
 			if (i / 10 != a && i % 10 != a) {
